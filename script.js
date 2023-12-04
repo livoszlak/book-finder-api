87ba6b8ef5583a2d3d0a15dce80bf4ce25778e8a
Liv <110914989+livoszlak@users.noreply.github.com>
2023-12-04 10:04:29 +0100

    More clean-up and added language selector

--- script.js
+++ script.js
@@ -2,34 +2,107 @@ window.onload = function () {
   const baseUrl = "https://www.googleapis.com/books/v1/volumes";
   const key = "AIzaSyCnjNdK4OUoRgi9fwcxHCSlJSg8TA6nPkA";
   const dataWrapper = document.getElementById("data-wrapper");
-
+  const clearButton = document.getElementById("clear-button");
+  const searchButton = document.getElementById("search-button");
   let search = document.getElementById("search-term");
+  let uniqueBooks = [];
+
+  const languages = [
+    { name: "Amharic", lang: "am" },
+    { name: "Arabic", lang: "ar" },
+    { name: "Basque", lang: "eu" },
+    { name: "Bengali", lang: "bn" },
+    { name: "English (UK)", lang: "en" },
+    { name: "Portuguese (Brazil)", lang: "pt" },
+    { name: "Bulgarian", lang: "bg" },
+    { name: "Catalan", lang: "ca" },
+    { name: "Cherokee", lang: "chr" },
+    { name: "Croatian", lang: "hr" },
+    { name: "Czech", lang: "cs" },
+    { name: "Danish", lang: "da" },
+    { name: "Dutch", lang: "nl" },
+    { name: "English (US)", lang: "en" },
+    { name: "Estonian", lang: "et" },
+    { name: "Filipino", lang: "fil" },
+    { name: "Finnish", lang: "fi" },
+    { name: "French", lang: "fr" },
+    { name: "German", lang: "de" },
+    { name: "Greek", lang: "el" },
+    { name: "Gujarati", lang: "gu" },
+    { name: "Hebrew", lang: "iw" },
+    { name: "Hindi", lang: "hi" },
+    { name: "Hungarian", lang: "hu" },
+    { name: "Icelandic", lang: "is" },
+    { name: "Indonesian", lang: "id" },
+    { name: "Italian", lang: "it" },
+    { name: "Japanese", lang: "ja" },
+    { name: "Kannada", lang: "kn" },
+    { name: "Korean", lang: "ko" },
+    { name: "Latvian", lang: "lv" },
+    { name: "Lithuanian", lang: "lt" },
+    { name: "Malay", lang: "ms" },
+    { name: "Malayalam", lang: "ml" },
+    { name: "Marathi", lang: "mr" },
+    { name: "Norwegian", lang: "no" },
+    { name: "Polish", lang: "pl" },
+    { name: "Portuguese (Portugal)", lang: "pt" },
+    { name: "Romanian", lang: "ro" },
+    { name: "Russian", lang: "ru" },
+    { name: "Serbian", lang: "sr" },
+    { name: "Chinese (PRC)", lang: "zh" },
+    { name: "Slovak", lang: "sk" },
+    { name: "Slovenian", lang: "sl" },
+    { name: "Spanish", lang: "es" },
+    { name: "Swahili", lang: "sw" },
+    { name: "Swedish", lang: "sv" },
+    { name: "Tamil", lang: "ta" },
+    { name: "Telugu", lang: "te" },
+    { name: "Thai", lang: "th" },
+    { name: "Chinese (Taiwan)", lang: "zh" },
+    { name: "Turkish", lang: "tr" },
+    { name: "Urdu", lang: "ur" },
+    { name: "Ukrainian", lang: "uk" },
+    { name: "Vietnamese", lang: "vi" },
+    { name: "Welsh", lang: "cy" },
+  ];
+
+  const createLanguageOptions = (languages) => {
+    const langSelect = document.getElementById("language");
+    languages.forEach((language) => {
+      const langOption = document.createElement("option");
+      langOption.value = language.lang;
+      langOption.setAttribute("name", language.name);
+      langOption.innerHTML = language.name;
+      langSelect.appendChild(langOption);
+    });
+  };
+
+  createLanguageOptions(languages);
 
   const clear = () => {
     dataWrapper.innerHTML = "";
   };
 
-  const clearButton = document.getElementById("clear-button");
   clearButton.addEventListener("click", () => {
     clear();
   });
 
-  const searchAndFetchData = (searchValue, searchType) => {
+  const searchAndFetchData = (searchValue, searchType, selectedLanguage) => {
     clear();
-    fetchData(searchValue, searchType);
+    fetchData(searchValue, searchType, selectedLanguage);
     search.value = "";
   };
 
-  const searchButton = document.getElementById("search-button");
   searchButton.addEventListener("click", () => {
     let searchType = document.getElementById("search-type").value;
     let searchValue = search.value;
+    let selectedLanguage = document.getElementById("language").value;
     if (searchType === "author") {
       let authorName = searchValue;
       let authorLastName = authorName.split(" ").pop();
-      searchAndFetchData(authorLastName, searchType);
+      searchAndFetchData(authorLastName, searchType, selectedLanguage);
     } else {
-      searchAndFetchData(searchValue, searchType);
+      searchAndFetchData(searchValue, searchType, selectedLanguage);
     }
   });
 
@@ -37,7 +110,15 @@ window.onload = function () {
     if (keySearch.key === "Enter") {
       keySearch.preventDefault();
       let searchType = document.getElementById("search-type").value;
-      searchAndFetchData(search.value, searchType);
+      let searchValue = search.value;
+      let selectedLanguage = document.getElementById("language").value;
+      if (searchType === "author") {
+        let authorName = searchValue;
+        let authorLastName = authorName.split(" ").pop();
+        searchAndFetchData(authorLastName, searchType, selectedLanguage);
+      } else {
+        searchAndFetchData(searchValue, searchType, selectedLanguage);
+      }
     }
   });
 
@@ -71,7 +152,7 @@ window.onload = function () {
     return 0;
   };
 
-  const fetchData = async (searchValue, searchType) => {
+  const fetchData = async (searchValue, searchType, language) => {
     let queryParam = "";
     if (searchType === "title") {
       queryParam = "intitle:" + encodeURIComponent(searchValue);
@@ -84,6 +165,8 @@ window.onload = function () {
         "?q=" +
         queryParam +
         "&maxResults=40" +
+        "&langRestrict=" +
+        language +
         "&printType=books" +
         "&fields=items(volumeInfo/title,volumeInfo/authors,volumeInfo/imageLinks,volumeInfo/canonicalVolumeLink)" +
         "&key=" +
@@ -93,8 +176,6 @@ window.onload = function () {
       }
     );
     const data = await response.json();
-
-    let uniqueBooks = [];
 
     data.items.forEach((item) => {
       if (
@@ -160,7 +241,6 @@ window.onload = function () {
     } else if (sortType === "author") {
       uniqueBooks.sort(sortByAuthor);
     }
-
     displayData(uniqueBooks);
   };
 };
@@ -201,7 +281,7 @@ const createBookCoverWrapper = () => {
   return bookCoverWrapper;
 };
 
-const createBookCover = (book, bookTitle, bookAuthor, bookWrapper) => {
+const createBookCover = (book, bookTitle, bookAuthor) => {
   const bookCover = document.createElement("img");
   let coverUrl = book["volumeInfo"]["imageLinks"]["thumbnail"];
   let noEdgeCurlUrl = coverUrl.replace(/&edge=curl/g, "");
